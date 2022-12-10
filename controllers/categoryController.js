3262f6229f647deb3b4fe1d5246a0c4390e7056c
satoshi-sh <satoss1108@gmail.com>
2022-12-10 15:24:34 -0600

    Add get all categories

---
+++ controllers/categoryController.js
@@ -0,0 +1,15 @@
+const Category = require("../models/category")
+
+
+// get all categories 
+
+exports.all_categories = (req,res, next) =>{
+    Category.find({},)
+    .sort({createdAt:1})
+    .exec(function(err,all_categories){
+        if(err){
+            return next(err)
+        }
+        res.json(all_categories)
+    })
+}
--- routes/blogapiRoutes.js
+++ routes/blogapiRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router();
 const blogController = require('../controllers/blogController')
+const categoryController = require('../controllers/categoryController')
 
 router.get('/',blogController.all_posts)
+
+router.get('/categories',categoryController.all_categories)
+
+
 router.get('/:id',blogController.get_post)
+
+
 
 module.exports = router
