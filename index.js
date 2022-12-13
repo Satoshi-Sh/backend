db61d2eb08b679d23bcd3d6c4390003009367242
satoshi-sh <satoss1108@gmail.com>
2022-12-12 18:07:00 -0600

    Change cors origin

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(helmet())
 
 // cors setting 
 const corsOption = {
-    origin:'https://satoshi-sh.github.io/portfolio/',
+    origin:'https://satoshi-sh.github.io',
     credentials:true,
     optionSuccessStatus:200
 }
