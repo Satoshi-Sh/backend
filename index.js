fdba02fc855e5831374705bc3170f2ea88a38cc7
satoshi-sh <satoss1108@gmail.com>
2023-06-07 10:45:08 -0500

    Add another url to corsOption origin

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
 const express = require("express");
 const connectDB = require("./mongoConnect");
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
-const blogapiRouter = require('./routes/blogapiRoutes')
-const authRouter = require('./routes/authRoutes')
-const path = require('path')
-const cors = require('cors')
-const compression = require("compression")
-const helmet = require("helmet")
-
+const blogapiRouter = require("./routes/blogapiRoutes");
+const authRouter = require("./routes/authRoutes");
+const path = require("path");
+const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
-require('./passport')
+require("./passport");
 
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Enable body parser 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(compression());
+app.use(helmet());
 
-app.use(compression())
-app.use(helmet())
-
-// cors setting 
+// cors setting
 const corsOption = {
-    origin:'https://satoshi-sh.github.io',
-    credentials:true,
-    optionSuccessStatus:200
-}
+  origin: ["https://satoshi-sh.github.io", "http://satoshis-developer.xyz"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOption))
+app.use(cors(corsOption));
 
+// set static folder
 
-// set static folder 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use("/blogapi", blogapiRouter);
+app.use("/auth", authRouter);
 
-
-
-
-app.use('/blogapi', blogapiRouter)
-app.use('/auth',authRouter)
-
-
-connectDB().then(()=>{app.listen(port,()=>{
-    console.log(`Server started on port ${port}`)
-})
-})
-
-
-
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+});
